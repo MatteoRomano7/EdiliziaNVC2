70db268c048c296aab22797b974baee4744c8e66
Matteo <ezmatteo@aol.com>
2024-02-27 16:33:23 +0100

    Aggiungere i file di progetto.

---
+++ Models/Db.cs
@@ -0,0 +1,41 @@
+﻿namespace EdiliziaCompleto.Models
+{
+    public static class Db
+    {
+        private static int _maxId = 3;
+
+        private static List<Dipendente> _dipendenti { get; set; } = [
+            new Dipendente() {DipendenteId = 1, Nome = "Francesco", Cognome = "Missorini", Indirizzo = "Via Brombeis", CodiceFiscale = "1234567890123456", Coniugato = true, Figli = 3, Mansione = "Scafista"},
+            new Dipendente() {DipendenteId = 2, Nome = "Abel", Cognome = "Sassu", Indirizzo = "Via Cagliari", CodiceFiscale = "1234567890123456", Coniugato = true, Figli = 5, Mansione = "Guardia"},
+            new Dipendente() {DipendenteId = 3, Nome = "Marco", Cognome = "Silveri", Indirizzo = "Via Roma", CodiceFiscale = "1234567890123456", Coniugato = true, Figli = 2, Mansione = "Professore"},
+            ];
+
+        public static List<Dipendente> getAll()
+        {
+            return _dipendenti;
+        }
+
+        public static Dipendente? getById(int? id) 
+        {
+            if (id == null) return null;
+
+            for (int i =0; i < _dipendenti.Count; i++)
+            {
+                Dipendente dipendente = _dipendenti[i];
+                if (_dipendenti[i].DipendenteId == id)
+                {
+                    return dipendente;
+                };
+            }
+            return null;    
+        }
+        public static Dipendente AggiungiDipendente(string nome, string cognome, string indirizzo, string codicefiscale, bool coniugato, int figli, string mansione) 
+        {
+            _maxId++;
+            var dipendente = new Dipendente() { DipendenteId = _maxId, Nome = nome, Cognome = cognome, Indirizzo = indirizzo, CodiceFiscale = codicefiscale, Coniugato = coniugato, Figli = figli, Mansione = mansione };
+            _dipendenti.Add(dipendente);
+            return dipendente;
+        } 
+    }
+
+}
---
+++ Models/Dipendente.cs
@@ -0,0 +1,15 @@
+﻿namespace EdiliziaCompleto.Models
+{
+    public class Dipendente
+    {
+        public int DipendenteId { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Indirizzo { get; set; }
+        public string CodiceFiscale { get; set; }
+        public bool Coniugato { get; set; }
+        public int Figli { get; set; }
+
+        public string Mansione { get; set; }
+    }
+}
